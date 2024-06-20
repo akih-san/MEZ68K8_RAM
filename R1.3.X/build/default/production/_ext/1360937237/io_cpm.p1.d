36d865df7041d583ee555d1840be9c3f6a20068b
@Akih_san <112370246+akih-san@users.noreply.github.com>
2024-06-20 22:34:00 +0900

    Add files via upload

---
+++ R1.3.X/build/default/production/_ext/1360937237/io_cpm.p1.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1360937237/io_cpm.p1:  \
+../src/io_cpm.c  \
+../src/../src/mez68k8.h  \
+../src/../src/picconfig.h  \
+../src/../fatfs/ff.h  \
+../src/../fatfs/../fatfs/ffconf.h  \
+../src/../drivers/utils.h 
---
+++ R1.3.X/build/default/production/_ext/1360937237/mez68k8_main.p1.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1360937237/mez68k8_main.p1:  \
+../src/mez68k8_main.c  \
+../src/../src/mez68k8.h  \
+../src/../src/picconfig.h  \
+../src/../fatfs/ff.h  \
+../src/../fatfs/../fatfs/ffconf.h  \
+../src/../drivers/utils.h 
